76e9d544ec949aa90b758c12a5b5420534820442
Artem Sorokin <artem@sorokin.pp.ru>
2024-04-23 11:22:54 +0300

    Добавлено базовое тестирование на корректность импорта

---
+++ tests/test_backward.py
@@ -0,0 +1,17 @@
+def test_backward() -> None:
+    from custom_components.yandex_station_intents import async_setup
+    from custom_components.yandex_station_intents.config_flow import YandexSmartHomeIntentsFlowHandler
+    from custom_components.yandex_station_intents.media_player import YandexStationIntentMediaPlayer
+    from custom_components.yandex_station_intents.yandex_intent import IntentManager
+    from custom_components.yandex_station_intents.yandex_quasar import YandexQuasar
+    from custom_components.yandex_station_intents.yandex_session import YandexSession
+
+    for o in [
+        async_setup,
+        YandexSmartHomeIntentsFlowHandler,
+        YandexStationIntentMediaPlayer,
+        IntentManager,
+        YandexQuasar,
+        YandexSession,
+    ]:
+        assert o
